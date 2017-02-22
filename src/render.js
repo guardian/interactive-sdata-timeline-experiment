7f7c5ae28111bb1b54cfa2927d9e74903a24d872
Mariot Chauvin <mariot.chauvin@gmail.com>
2017-02-22 13:46:03 +0000

    View is now a proper template

--- src/render.js
+++ src/render.js
@@ -6,6 +6,15 @@ export async function render() {
 
     const template = Handlebars.compile(mainTemplate);
     //TODO retrieve the data from spreadsheet of put it there for experimentation
-    const data = {};
+    const data = {
+        timestamp: [
+            { value: '2013', isYear: true},
+            { value: 'July', isYear: false, isActive: true},
+            { value: 'August', isYear: false},
+            { value: '2014', isYear: true},
+            { value: '2015', isYear: true},
+            { value: '2016', isYear: true}
+        ]
+    };
     return template(data);
 }
