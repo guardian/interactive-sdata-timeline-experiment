6465be2f1fcabdeb78471f2177dfcb543e0b50a1
Mariot Chauvin <mariot.chauvin@gmail.com>
2017-02-23 12:08:33 +0000

    Order articles by category

--- src/render.js
+++ src/render.js
@@ -15,10 +15,31 @@ export async function render() {
                 title: 'Discovery that 90% of staff are on zero hour contracts',
                 who: ['Mike Ashley', 'Labour Party', 'The Guardian'],
                 options: [],
-                articles: [
-                    {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
-                    {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
-                    {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'}
+                articles:[
+                    {
+                    category: "News",
+                    data: [
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'}
+                     ]
+                    },
+                    {
+                    category: "Analysis",
+                    data: [
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'}
+                     ]
+                    },
+                    {
+                    category: "Opinions",
+                    data: [
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'},
+                        {title: 'Sports Direct auditor under investigation', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal', when: '28/10'}
+                     ]
+                    }
                 ]
             },
             { when: 'August'},
