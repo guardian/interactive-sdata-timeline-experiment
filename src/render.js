546dd27000285b30f9b8244b34d9e95ddd46f272
Mariot Chauvin <mariot.chauvin@gmail.com>
2017-02-23 11:26:32 +0000

    Add more links

--- src/render.js
+++ src/render.js
@@ -7,13 +7,26 @@ export async function render() {
     const template = Handlebars.compile(mainTemplate);
     //TODO retrieve the data from spreadsheet of put it there for experimentation
     const data = {
-        timestamp: [
-            { value: '2013', isYear: true},
-            { value: 'July', isYear: false, isActive: true},
-            { value: 'August', isYear: false},
-            { value: '2014', isYear: true},
-            { value: '2015', isYear: true},
-            { value: '2016', isYear: true}
+        events: [
+            { when: '2013', isYear: true},
+            { 
+                when: 'July',
+                isActive: true,
+                title: 'Discovery that 90% of staff are on zero hour contracts',
+                who: ['Mike Ashley', 'Labour Party', 'The Guardian'],
+                options: [],
+                articles: [
+                    {title: 'Sports Direct auditor under investigation over company\'s family deal', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal'},
+                    {title: 'Sports Direct auditor under investigation over company\'s family deal', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal'},
+                    {title: 'Sports Direct auditor under investigation over company\'s family deal', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal'},
+                    {title: 'Sports Direct auditor under investigation over company\'s family deal', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal'},
+                    {title: 'Sports Direct auditor under investigation over company\'s family deal', link: 'https://www.theguardian.com/business/2016/nov/28/sports-direct-auditor-under-investigation-over-companys-family-deal'}
+                ]
+            },
+            { when: 'August'},
+            { when: '2014', isYear: true},
+            { when: '2015', isYear: true},
+            { when: '2016', isYear: true}
         ]
     };
     return template(data);
